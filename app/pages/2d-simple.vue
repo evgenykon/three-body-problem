af27d45d75ba1d89e9c610abbf187ee49286ffbe
evgeny.konoplev <evgeny.konoplev@yaizy.io>
2026-05-11 04:25:26 +0300

    ui

---
+++ app/pages/2d-simple.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default'
+})
+</script>
+
+<template>
+  <UiContainer>
+    <UiHeader :level="1" class="mb-8">2D Simple Simulation</UiHeader>
+    
+    <UiCard>
+      <UiCardContent>
+        <p>Simulation content coming soon...</p>
+      </UiCardContent>
+    </UiCard>
+  </UiContainer>
+</template>
